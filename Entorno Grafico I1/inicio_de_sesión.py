5c52af384e9d6491e434e9e5dd2cdd508bc4656c
BryanDeAnda <71561585+BryanDeAnda@users.noreply.github.com>
2021-11-14 19:36:17 -0600

    Add files via upload

---
+++ Entorno Grafico I1/inicio_de_sesión.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'inicio_de_sesión.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import sys
+from PyQt5.QtGui import QPixmap
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(1198, 799)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("icono.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        self.bgwidget = QtWidgets.QWidget(Dialog)
+        self.bgwidget.setGeometry(QtCore.QRect(0, 0, 1201, 801))
+        self.bgwidget.setStyleSheet("\n"
+"\n"
+"")
+        self.bgwidget.setObjectName("bgwidget")
+        self.image = QtWidgets.QLabel(self.bgwidget)
+        self.image.setGeometry(QtCore.QRect(0, 0, 1201, 801))
+        self.image.setStyleSheet("border-radius:50%;")
+        self.image.setText("")
+        self.image.setScaledContents(True)
+        self.image.setObjectName("image")
+        self.image.setPixmap(QPixmap('fondoiniciofuerte.jpg'))
+        self.label_2 = QtWidgets.QLabel(self.bgwidget)
+        self.label_2.setGeometry(QtCore.QRect(370, 110, 531, 101))
+        self.label_2.setStyleSheet("font: italic 40pt \"Brush Script MT\"; color:rgb(255, 255, 255)\n"
+"")
+        self.label_2.setObjectName("label_2")
+        self.login_2 = QtWidgets.QPushButton(self.bgwidget)
+        self.login_2.setGeometry(QtCore.QRect(560, 530, 121, 41))
+        self.login_2.setStyleSheet("border-radius:16px;\n"
+"font: 8pt \"MS Shell Dlg 2\";\n"
+"background-color:rgb(98, 80, 127);\n"
+"font: 11pt \"MS Shell Dlg 2\";color: rgb(255, 255, 255)")
+        self.login_2.setObjectName("login_2")
+        self.label_5 = QtWidgets.QLabel(self.bgwidget)
+        self.label_5.setGeometry(QtCore.QRect(450, 230, 71, 21))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI Black")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(10)
+        self.label_5.setFont(font)
+        self.label_5.setStyleSheet("color: rgb(255, 255, 255);\n"
+"font: 87 12pt \"Segoe UI Black\";")
+        self.label_5.setObjectName("label_5")
+        self.emailfield_2 = QtWidgets.QLineEdit(self.bgwidget)
+        self.emailfield_2.setGeometry(QtCore.QRect(460, 250, 341, 51))
+        self.emailfield_2.setStyleSheet("background-color:rgba(0,0,0,0);\n"
+"font: 12pt \"MS Shell Dlg 2\";color:rgb(235, 235, 235);")
+        self.emailfield_2.setText("")
+        self.emailfield_2.setObjectName("emailfield_2")
+        self.label_6 = QtWidgets.QLabel(self.bgwidget)
+        self.label_6.setGeometry(QtCore.QRect(460, 380, 111, 21))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI Black")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(10)
+        self.label_6.setFont(font)
+        self.label_6.setStyleSheet("; color: rgb(255, 255, 255);\n"
+"font: 87 12pt \"Segoe UI Black\";")
+        self.label_6.setObjectName("label_6")
+        self.passwordfield = QtWidgets.QLineEdit(self.bgwidget)
+        self.passwordfield.setGeometry(QtCore.QRect(460, 400, 341, 51))
+        self.passwordfield.setStyleSheet("background-color:rgba(0,0,0,0);\n"
+"font: 12pt \"MS Shell Dlg 2\";color:rgb(235, 235, 235);")
+        self.passwordfield.setText("")
+        self.passwordfield.setObjectName("passwordfield")
+        self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.label = QtWidgets.QLabel(self.bgwidget)
+        self.label.setGeometry(QtCore.QRect(510, 470, 241, 31))
+        self.label.setStyleSheet("color: rgb(255, 8, 12);\n"
+"font: 11pt \"MS Shell Dlg 2\";")
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Iluminación Residencial"))
+        self.label_2.setText(_translate("Dialog", "Iluminación Residencial"))
+        self.login_2.setText(_translate("Dialog", "Ingresar"))
+        self.label_5.setText(_translate("Dialog", "Admin"))
+        self.label_6.setText(_translate("Dialog", "Password"))
+        self.label.setToolTip(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:11pt; font-weight:600;\">Admin o Password incorrectos</span></p></body></html>"))
+        self.label.setWhatsThis(_translate("Dialog", "<html><head/><body><p align=\"center\"><br/></p></body></html>"))
+        self.label.setText(_translate("Dialog", "Admin o Password incorrectos"))
+
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
